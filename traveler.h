3d7dcf4aed4f57ddf11d8f0c2e6d928b84d1e7f0
Sean <SeanLynn@users.noreply.github.com>
2016-03-08 23:03:29 +0800

    Update traveler.h

--- traveler.h
+++ traveler.h
@@ -11,6 +11,7 @@
 #include <sstream>
 #include <cstdlib>
 #include <map>
+#include <deque>
 using namespace std;
 typedef enum { CANUSE, INSTACK, INQUEUE } VertexStatus;
 typedef enum {RECORDED, AVAILABLE} EdgeStatus;
@@ -28,6 +29,7 @@ public:
 	int getEndId() { return endId; }//获得入射顶点编号
 	int getWeight() { return weight; }//获得
 	EdgeStatus getStatus() { return eStatus; }
+	void setStatus(EdgeStatus status) { eStatus = status; }
 };
 class vertex {
 private:
@@ -44,10 +46,18 @@ public:
 	void setStatus(VertexStatus s) { vStatus = s; }
 	VertexStatus status() { return vStatus; }//获得顶点的状态
 	void newEdge(edge* e) { vec_edge.push_back(e); }//创建新的边
-	vector<edge*>& getEdge() { return vec_edge; }//获得边的vector
+	vector<edge*>& getEdgeOfVector() { return vec_edge; }//获得边的vector
 	int getEdgeQuantity() { return vec_edge.size(); }//获取出弧的数量
-	EdgeStatus getEdgeStatus(int n) { return vec_edge[n]->getStatus(); }
-
+	bool AVIEdgeStatus(int n) {
+		if(n < vec_edge.size()) return vec_edge[n]->getStatus()==AVAILABLE;
+		else return false;
+	}
+	bool AVIVertexStatus(deque<int> path, int n) {
+		if (find(path.begin(), path.end(), n) != path.end()) return false;
+		return true;
+	}
+	int nextVertexId(int n) { return vec_edge[n]->getEndId(); }
+	edge* getEdge(int n) { return vec_edge[n]; }
 };
 class graph {
 private:
@@ -57,33 +67,89 @@ private:
 	int start_id;//起点
 	int end_id;//终点
 	int MinWeight;//最小权重值，用作比较
-	set<vertex*> MustPassing;//必须经过的点
+	vector<vertex*> MustPassing;//必须经过的点
 	int num_v;//顶点数量
 	int	num_e;//边数量
+	vector<int>* final_path;//边路径
+	//deque<int>* along_path;
 public:
 	graph(const char* gfile, const char* pfile);//构造函数
 	//vector<int>& split(const string &str, char delim, vector<int> &elems_int);
 	vector<int>& split(const string &str, const string &delimiters, vector<int> &elems_int);//用于多个delimiters
 	void print();//用来打印图的信息
-	stack<edge>* DFS_shortPath(int start, int end);//深度优先搜索最短路径
-	vertex* nextVertex(vertex* topVertex);
-	void dfs_shortPath(vertex* currentVertex, int outdegree);
+	void DFS_shortPath(vector<edge*>* path);//深度优先搜索最短路径
+	void nextVertex(vertex* topVertex, int& outdegree);
+	void dfs_shortPath(vertex* currentVertex, int outdegree, vector<int>* path, vector<edge*>* edgePath);//递归的部分， 是否需要传deque<int>* 存疑,还是不传快？
+	void judgePath(vector<int>* vertexPath, vector<edge*>* edgePath);
+	void printFinalPath();
 };
-stack<edge>* graph::DFS_shortPath(int start, int end){
-	stack<int> pathOfVertexStack;//保存路径
-	pathOfVertexStack.push(start);//将起点索引放入路径栈中
-	vertex* topVertex = Map.find(start)->second;
+
+void graph::DFS_shortPath(vector<edge*>* edgePath){
+	vector<int>* vertexPath = nullptr;
+	vertexPath->push_back(start_id);//将起点索引放入点路径
+	vertex* topVertex = Map.find(start_id)->second;
 	topVertex->setStatus(INSTACK);//设置起点状态
 	int outdegree = 0;//用来作为访问弧终点的索引
 	while (outdegree < topVertex->getEdgeQuantity()) {
-		if (AVAILABLE == topVertex->getEdgeStatus(outdegree)) {
-			dfs_shortPath(topVertex, outdegree);
+		//if (topVertex->AVIEdgeStatus(outdegree)) {
+			dfs_shortPath(topVertex, outdegree, vertexPath, edgePath);
+		//}
+			outdegree++;
+	}
+}
+void graph::dfs_shortPath(vertex * currentVertex, int outdegree, vector<int>* vertexPath, vector<edge*>* edgePath){
+	//判断是否尾递归
+	vertex* nextVertex = Map.find(currentVertex->nextVertexId(outdegree))->second;//获取弧终点
+	if (nextVertex->VId() != end_id) {//不是终点
+		if (nextVertex->getEdgeQuantity() > 0) {//出度大于0
+			nextVertex->setStatus(INSTACK);
+			edge* nextEdge = currentVertex->getEdge(outdegree);
+			nextEdge->setStatus(RECORDED);
+			vertexPath->push_back(nextVertex->VId());
+			edgePath->push_back(nextEdge);
+			for (int i = 0; i < nextVertex->getEdgeQuantity(); i++)
+				if (Map.find(nextVertex->getEdge(i)->getEndId())->second->status() == CANUSE)
+					dfs_shortPath(nextVertex, i, vertexPath, edgePath);
+		}
+		return;
+	}
+	else {
+		vertexPath->push_back(nextVertex->VId());
+		edgePath->push_back(nextVertex->getEdge(outdegree));
+		judgePath(vertexPath, edgePath);
+		vertexPath->pop_back();
+		edgePath->pop_back();
+		return;
+	}
+}
+void graph::judgePath(vector<int>* vertexPath, vector<edge*>* edgePath){//当路径尾是终点时会调用此函数
+	int p_size = MustPassing.size();
+	if (p_size < vertexPath->size()) {//路径节点数要大于path中节点数量
+		for (vector<vertex*>::iterator it = MustPassing.begin(); it != MustPassing.end(); it++)
+			if (find(vertexPath->begin(), vertexPath->end(), (*it)->VId()) == vertexPath->end())
+				return;//检查是否vertexPath包含MustPassing
+		int p_weight = 0;//所要比较的路径的总权重
+		for (vector<edge*>::iterator it = edgePath->begin(), ed = edgePath->end(); it != ed; it++) 
+			p_weight += (*it)->getWeight();//计算所要比较的路径的总权重
+		if (final_path->empty() || p_weight < MinWeight) {//如果图类中的路径存在
+			final_path->clear();//edgePath是更好地路径
+			for (vector<edge*>::iterator it = edgePath->begin(), ed = edgePath->end(); it != ed; it++)
+				final_path->push_back((*it)->getEId());
 		}
 	}
-	return NULL;
+	return;
 }
-vertex* graph::nextVertex(vertex* topVertex) {
-
+void graph::nextVertex(vertex* topVertex, int& outdegree) {
+	while (outdegree < topVertex->getEdgeQuantity()) {
+		if (topVertex->AVIEdgeStatus(outdegree))
+			break;
+		outdegree++;
+	}
+}
+void graph::printFinalPath(){
+	cout << final_path->at(0);
+	for (int i = 1; i < final_path->size(); i++)
+		cout << '|' << final_path->at(i);
 }
 graph::graph(const char* gfile, const char* pfile) {
 //构造函数graph的作用，接受两个.csv文件的路径，初始化点集和边集，初始化MustPassing点集，
@@ -126,9 +192,13 @@ graph::graph(const char* gfile, const char* pfile) {
 	start_id = int_from_file[0];
 	end_id = int_from_file[1];//起点和终点一定在第一二位置上
 	for (int i = 2; i < int_from_file.size(); i++) {
-		MustPassing.insert(Map.find(int_from_file[i])->second);
-	}//查找并添加到MustPassing中去
+		MustPassing.push_back(Map.find(int_from_file[i])->second);
+	}//查找并添加到MustPassing中去 
 	outFile.close();
+	final_path = nullptr;
+	MinWeight = 0;
+	vector<edge*>* edgePath = nullptr;
+	DFS_shortPath(edgePath);
 }
 //vector<int>& graph::split(const string &str, char delim, vector<int> &elems_int) {
 //	istringstream iss(str);
@@ -167,7 +237,7 @@ void graph::print()//打印图的一些信息
 		cout << "vertex's id: " << it->first << "    status:" << it->second->status() << endl;
 		cout << "\tedge: " << endl;
 		int i = 0;
-		for (vector<edge*> et = it->second->getEdge(); i < et.size(); i++) {
+		for (vector<edge*> et = it->second->getEdgeOfVector(); i < et.size(); i++) {
 			cout << "\tedge's id " << et[i]->getEId() << " endVId: " << et[i]->getEndId() << "weight: " << et[i]->getWeight() <<endl;
 		}
 		cout << endl;
